2ee7391be156d23d97cb7f82a1e30828e3f50247
yccjs <yccjsalvador@gmail.com>
2022-05-12 08:58:44 +0900

    chore:deploy attempt

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.use(router);
 app.use(express.json());
 const PORT = process.env.PORT || 8080;
 
-app.listen(8080, () =>
+app.listen(PORT, () =>
   console.log(`server is running,  the app is listening on port ${PORT}`)
 );
