69134fffc1f8014793390b143a0122204213621e
yccjs <yccjsalvador@gmail.com>
2022-05-12 18:07:32 +0900

    feat:implement post request

--- routes/post.js
+++ routes/post.js
@@ -4,6 +4,7 @@ const knex = require("knex")(config);
 
 const router = express.Router();
 const cors = require("cors");
+const res = require("express/lib/response");
 router.use(cors());
 
 require("dotenv").config({
@@ -13,6 +14,18 @@ require("dotenv").config({
 router.get("/", async (req, res) => {
   const data = await knex.select("*").from("posts");
   res.status(200).send(data);
+});
+
+router.post("/newpost", async (req, res) => {
+  const postObj = {
+    username: req.body.username,
+    image: req.body.image,
+    cocktail_name: req.body.cocktail_name,
+    description: req.body.description,
+    recipe: req.body.recipe,
+  };
+  console.log("saving", postObj);
+  await knex("posts").insert(postObj);
 });
 
 module.exports = router;
